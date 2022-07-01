19a4533eaa0b4cd98cb6dba8e479be0f405012db
devstar0611 <devstar0611@gmail.com>
2022-07-01 02:20:54 -0700

    test

--- scraper.py
+++ scraper.py
@@ -60,15 +60,6 @@ def get_price_name(target_url):
     product_upc = 'Not Found'
     product_imageurl = 'Not Found'
     
-    page = urlopen(target_url)
-    html = page.read().decode("utf-8")
-    
-    soup = BeautifulSoup(html, "html.parser")
-    span_categories = soup.find_all("span", attrs={"class": "CellSkinny__TextWrapper-sc-117d15w-0 bcbdds"})
-    for category in span_categories:
-        print(category.string)
-        print(category.parent.parent["href"])
-    
     try:
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         driver.get(target_url)
--- scraper_test.py
+++ scraper_test.py
@@ -2,7 +2,7 @@ from attr import attr
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-TARGET_HOME = "https://www.target.com/"
+TARGET_HOME = "https://www.target.com"
 
 target_url = TARGET_HOME
 
@@ -10,10 +10,6 @@ page = urlopen(target_url)
 html = page.read().decode("utf-8")
 
 soup = BeautifulSoup(html, "html.parser")
-categoryMenu = soup.find("div", attrs={"data-test": "@web/CategoryMenu"})
-print(type(categoryMenu[0]))
-span_categories = categoryMenu.find_all(
-    "span", attrs={"class": "CellSkinny__TextWrapper-sc-117d15w-0 bcbdds"})
-for category in span_categories:
-    print(category.string)
-    print(category.parent.parent["href"])
+body = soup.body
+print(body)
+
