e09e81b7cfd24563dd721bf3e9a69793192aec11
devstar0611 <devstar0611@gmail.com>
2022-07-01 01:23:09 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -64,7 +64,10 @@ def get_price_name(target_url):
     html = page.read().decode("utf-8")
     
     soup = BeautifulSoup(html, "html.parser")
-    print(soup.prettify())
+    span_categories = soup.find_all("span", attrs={"class": "CellSkinny__TextWrapper-sc-117d15w-0 bcbdds"})
+    for category in span_categories:
+        print(category.string)
+        print(category.parent.parent["href"])
     
     try:
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
